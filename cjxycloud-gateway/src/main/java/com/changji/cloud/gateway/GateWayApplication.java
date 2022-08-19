e5e138fcb6cf145c7eb54979e5127ebedf030d92
xiaowenhao66 <571375739@qq.com>
2022-08-19 18:15:12 +0800

    docker

--- cjxycloud-gateway/src/main/java/com/changji/cloud/gateway/GateWayApplication.java
+++ cjxycloud-gateway/src/main/java/com/changji/cloud/gateway/GateWayApplication.java
@@ -1,8 +1,18 @@
-package com.changji.cloud.gateway;/**
+package com.changji.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * @author 小问号
- * @date 2022/8/19 17 
+ * @date 2022/8/19 17
  * description
  */
 
+
+@SpringBootApplication
 public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
 }
